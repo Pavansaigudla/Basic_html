97c7f28bcc00d52aaf8fd850cf905ddfdb8b2a3f
Pavansai Gudla <pavansaigudla.7@gmail.com>
2023-01-20 12:09:21 +0530

    new

--- JSFile.js
+++ JSFile.js
@@ -46,7 +46,7 @@ function paynow(){
     let Output= Result.value
     document.querySelector(".res1").textContent = Amount.value
     document.querySelector(".result").textContent = Output
-    aptrinsic('track','transaction',{"status":Output, "Amount" :Amount.value});
+    aptrinsic('track','transaction',{"status":Output, "Value" :Amount.value});
     alert("Transaction Done");
 }
 
