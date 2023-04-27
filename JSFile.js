a80ae7bff17d5c82fe1e6a093be1560900ebe05b
Pavansaigudla <122771635+Pavansaigudla@users.noreply.github.com>
2023-04-27 19:52:48 +0530

    Update JSFile.js

--- JSFile.js
+++ JSFile.js
@@ -84,7 +84,3 @@ function paynow(){
     aptrinsic('track','transaction',{"TransactionStatus":Output, "transactionAmount" :transactionAmount});
     alert("Transaction Done");
 }
-function set_attribute(){
-aptrinsic('set', 'user', {'firstName' : 'Eladtest', 'lastName' : 'Cohen', 'role': 'Software Engineer'});
-aptrinsic('track', 'Set_Test_Check', {'firstName' : 'Eladtest', 'lastName' : 'Cohen', 'role': 'Software Engineer'});
-}
