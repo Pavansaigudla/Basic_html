4d16d37bb35346e888c6e066d79d023b33033f57
Pavansaigudla <122771635+Pavansaigudla@users.noreply.github.com>
2024-08-30 06:03:26 +0530

    Update JSFile.js

--- JSFile.js
+++ JSFile.js
@@ -5,7 +5,7 @@
     }, n[i].p = e; n[i].c = co;
     var r = t.createElement("script"); r.async = !0, r.src = a + "?a=" + e;
     var c = t.getElementsByTagName("script")[0]; c.parentNode.insertBefore(r, c)
-})(window, document, "https://web-sdk.aptrinsic.com/api/aptrinsic.js", "AP-OM2DTW6NF8A7-2-1"); 
+})(window, document, "https://web-sdk.aptrinsic.com/api/aptrinsic.js", "AP-OM2DTW6NF8A7-2-3"); 
 
 
 
